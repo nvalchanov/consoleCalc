f8ed0c0b4d9ced887f1477bd0be4e3c95c4511e4
Nikola Valchanov <nvalchanov@uni-plovdiv.bg>
2026-01-06 16:26:21 +0200

    Implemented +, -, *, / operations

--- ConsoleCalc/ConsoleCalc/Program.cs
+++ ConsoleCalc/ConsoleCalc/Program.cs
@@ -3,4 +3,26 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("B: ");
 int b = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine($"A + B = {a + b}");
+Console.Write("Operation (+, -, *, /): ");
+string operation = Console.ReadLine();
+
+if(operation == "/")
+{
+    Console.WriteLine($"Result: {a / b}");
+}
+else if(operation == "+")
+{
+    Console.WriteLine($"Result: {a + b}");
+}
+else if(operation == "*")
+{
+    Console.WriteLine($"Result: {a * b}");
+}
+else if(operation == "-")
+{
+    Console.WriteLine($"Result: {a - b}");
+}
+else
+{
+    Console.WriteLine("Error: Invalid operation");
+}
