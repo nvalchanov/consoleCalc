025b888850351645636c5009d679b0dd5337e5b7
Nikola Valchanov <nvalchanov@gmail.com>
2026-01-06 16:22:58 +0200

    Implemented the calculations

--- ConsoleCalc/ConsoleCalc/Program.cs
+++ ConsoleCalc/ConsoleCalc/Program.cs
@@ -3,4 +3,32 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("B: ");
 int b = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine($"A + B = {a + b}");
+string op = "";
+Console.Write("Operation (+, -, *, /): ");
+op = Console.ReadLine();
+
+if (op == "+")
+{
+    Console.WriteLine($"A + B = {a + b}");
+}
+else if (op == "-")
+{
+    Console.WriteLine($"A - B = {a - b}");
+}
+else if (op == "*")
+{
+    Console.WriteLine($"A * B = {a * b}");
+}
+else if (op == "/")
+{
+    if (b != 0)
+    {
+        Console.WriteLine($"A / B = {a / b}");
+    }
+    else
+    {
+        Console.WriteLine("Error: Division by zero");
+    }
+}
+
+Console.ReadKey(true);
